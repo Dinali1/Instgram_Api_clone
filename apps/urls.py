e6963fa87c660cddda78457825c717e0a213d758
web <razakovshohrux@gmail.com>
2025-05-08 18:14:43 +0500

    task

--- apps/urls.py
+++ apps/urls.py
@@ -3,6 +3,8 @@ from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
 
 from apps.views import RegisterView, ProfileView, ProfileUpdateView
 
+from apps.views import AllStoriesApiView, CreateStoriesApiView, GetUsersStoriesAPIView, ViewsUserAPIView
+
 urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
@@ -11,4 +13,11 @@ urlpatterns = [
 
     path('profile/<username>/', ProfileUpdateView.as_view(), name='profile-update'),
 
+]
+
+urlpatterns += [
+    path('api/stories', AllStoriesApiView.as_view(), name='api_stories'),
+    path('api/create/stories', CreateStoriesApiView.as_view(), name='api_create_stories'),
+    path('api/stories/<int:pk>', GetUsersStoriesAPIView.as_view(), name='api_stories_pk'),
+    path('view/stories/<int:pk>', ViewsUserAPIView.as_view(), name='api_stories_view'),
 ]
