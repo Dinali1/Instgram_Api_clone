2ee13d76decaa027a1f3b8e5aaa7c8f83ad26618
izzat21-ui <masterfamily23@gmail.com>
2025-05-12 16:12:29 +0500

    Update files

--- apps/urls.py
+++ apps/urls.py
@@ -4,7 +4,7 @@ from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
 from apps.views import NotificationListView, TagsListView, NotificationMarkReadView
 
 urlpatterns = [
-    path('user/notification', NotificationListView.as_view(), name='notification-list'),
+    path('user/notification', NotificationListView.as_view(), name='get_notification'),
     path('user/notification/<int:id>/read', NotificationMarkReadView.as_view(), name='notification-list'),
     path('tags-list/<str:tag>/posts/', TagsListView.as_view(), name='tags-list'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
